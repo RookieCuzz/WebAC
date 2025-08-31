97ccd3c7b92ed02cd9a73645959e11a91eb549d2
RookieCuzz <85008507+RookieCuzz@users.noreply.github.com>
2025-08-31 23:21:11 +0800

    add: Swagger集成

---
+++ src/main/java/com/cuzz/webac/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.cuzz.webac.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot 3 API")
+                        .version("1.0")
+                        .description("Spring Boot 3 集成 Swagger 3 示例"));
+    }
+}
